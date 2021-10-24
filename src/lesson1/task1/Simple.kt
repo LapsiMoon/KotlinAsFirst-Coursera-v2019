0abb96f76b0371b230f09c4f544679ec8d1f0c22
Vladislav <kolesnikov2.va@edu.spbstu.ru>
2021-10-25 02:21:51 +0300

    Lesson 1: All tasks were solved

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -113,7 +113,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = initial.toDouble() * (1 + percent.toDouble() / 100).pow(3)
+fun accountInThreeYears(initial: Int, percent: Int): Double =
+    initial.toDouble() * (1 + percent.toDouble() / 100).pow(3)
 
 /**
  * Простая
